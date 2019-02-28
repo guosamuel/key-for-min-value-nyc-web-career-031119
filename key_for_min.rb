dcb067cec7e49be0df43f108b0374596361cb165
Samuel Guo <guosamuel1114@gmail.com>
2019-02-28 03:14:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,16 +6,24 @@ def key_for_min_value(name_hash)
     storage = []
     counter = 0
     min_value = 0
+    
     name_hash.each do |name, value|
       storage.push(value)
     end
-    while counter < storage.length
+    
+    while counter < storage.length - 1
       if storage[counter] > storage[counter+1]
         min_value = storage[counter+1]
         counter += 1
       end
     end
-  return min_value
+    
+    name_hash.each do |name, value|
+      if value == min_value
+        return name
+      end
+    end  
+    
   else
     return nil
   end
