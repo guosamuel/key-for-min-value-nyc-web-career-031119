78e2b8f4748260a049da4d677e4083d286c92044
Samuel Guo <guosamuel1114@gmail.com>
2019-02-28 03:40:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if name_hash
+  if !name_hash.empty?
     storage = []
     counter = 0
     
@@ -28,6 +28,6 @@ def key_for_min_value(name_hash)
     end  
     
   else
-    return name_hash.empty?
+    return nil
   end
 end
